30a91cdbbfda6f8eabd75d0df1167ff870e878b4
Nafiz Noyon <134128750+NafizNoyon@users.noreply.github.com>
2023-05-23 14:50:13 +0600

    Add files via upload

---
+++ c++/StringInput.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+    char name[20];
+    cout<<"Enter your name :";
+    gets (name);
+    cout<<"  welcome " <<name;
+
+
+
+
+
+    getch();
+}
