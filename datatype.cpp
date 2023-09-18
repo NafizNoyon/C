dc3221954dd7b9c110e8d51c06a908be09b53298
Nafiz Noyon <134128750+NafizNoyon@users.noreply.github.com>
2023-09-18 21:18:11 +0600

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int a,b;
+ a =10;
+ b= 20;
+ int sum = a+b;
+ cout<<"sum is:"<<sum <<endl;
+
+int div = b/a;
+cout<< "div is :"<<div<<endl;
+
+int sub = b-a;
+cout<<"sub is: " <<sub << endl;
+
+int mul =a*b;
+cout<<"mul is :" <<mul << endl;
+
+
+    getch();
+}
