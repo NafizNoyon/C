9ab1ef349674cb1e1629f5231c0b44bf1e0c3df9
Nafiz Noyon <134128750+NafizNoyon@users.noreply.github.com>
2023-09-24 20:18:50 +0600

    Add files via upload

---
+++ formatingoutput.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float  num1,num2;
+    num1=10;
+    num2=3;
+    cout<<"show point ";
+    float sum= num1+ num2;
+    cout<<"sum is: "<<sum <<endl;
+
+    float sub = num1 -num2;
+    cout<<"sub is : " <<sub <<endl;
+
+    float mul = num1 * num2;
+    cout <<" mul is : " <<mul <<endl;
+
+    float div = num1/num2;
+    cout<<" div is : "<<div <<endl;
+
+    return 0;
+
+}
