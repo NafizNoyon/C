6a1eaebff734fcdee8e7b7171648283cdd21e255
Nafiz Noyon <134128750+NafizNoyon@users.noreply.github.com>
2023-10-02 20:59:07 +0600

    Add files via upload

---
+++ oddevencheck.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int n,remainder ;
+    cout<<"Enter your number to be tested :";
+    cin >>n;
+    if(n>0)
+    {
+
+    if (n%2==0)
+    {
+        cout<<"inside if's body " <<endl;
+        cout<<"it is an even number " <<endl;
+    }
+    else
+    {
+        cout<<"Inside if's body" <<endl;
+        cout<<"it is an odd number "<<endl;
+
+    }
+    }
+    else
+    {
+        cout<<"you have entered a negative number " << endl;
+    }
+    return 0;
+}
+
